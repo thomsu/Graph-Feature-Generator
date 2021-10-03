80e66af6ac79a4459d5b8d6c74569ed05cb66689
Thom Su <thomsu.nyc@gmail.com>
2021-10-02 23:46:31 -0400

    initial commit

---
+++ graph_features.py
@@ -0,0 +1,12 @@
+
+class GraphInfo():
+
+    def __init__(self):
+        self._adj_matrix = None
+        self._floyd_warshall = None
+        self._degree_centrality = None
+        self._eigenvector_centrality = None
+        self._betweenness_centrality = None
+        self._closeness_centrality = None
+        self._katz_index = None
+        self._color_refinement = None
